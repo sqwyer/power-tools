f7269729e0d0f111eb29181d816cac57af446f02
Sawyer Bivens <sawyer.codes.stuff@gmail.com>
2022-02-18 15:54:50 -0600

    Push new changes

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -4,9 +4,20 @@ const findProject = require('./project').find;
 function get (req, res) {
     find(req.user.email, r => {
         if(r == undefined) return res.redirect('/');
-        else if(r.data.user.projects.length == 0) res.render(`${__dirname}/../../views/dashboard`, { user: r.data.user, projects: []});
+        else if(r.data.user.projects.length == 0 && r.data.user.invites.length == 0) res.render(`${__dirname}/../../views/dashboard`, { user: r.data.user, projects: [], invites: []});
         else {
             let projects = [];
+            if(r.data.user.projects.length == 0) {
+                let invites = [];
+                if(r.data.user.invites.length != 0) for(let k = 0; k < r.data.user.invites.length; k++) {
+                    findProject(r.data.user.invites[k], info2 => {
+                        if(info2.data) invites.push(info2.data)
+                        if(i+1==r.data.user.invites.length) res.render(`${__dirname}/../../views/dashboard`, { user: r.data.user, projects, invites });
+                    })
+                }
+                else res.render(`${__dirname}/../../views/dashboard`, { user: r.data.user, projects, invites });
+            } else {
+                let projects = [];
             for(let i = 0; i < r.data.user.projects.length; i++) {
                 findProject(r.data.user.projects[i], info => {
                     if(info.data) projects.push(info.data.project);
@@ -21,6 +32,7 @@ function get (req, res) {
                         else res.render(`${__dirname}/../../views/dashboard`, { user: r.data.user, projects, invites });
                     }
                 });
+            }
             }
         }
     });
