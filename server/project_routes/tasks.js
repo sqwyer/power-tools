ddd73030632559971d43e399217bc066f4d69405
Sawyer Bivens <sawyer.codes.stuff@gmail.com>
2022-03-01 10:32:50 -0600

    Add ability to update tasks

--- server/project_routes/tasks.js
+++ server/project_routes/tasks.js
@@ -73,11 +73,13 @@ function create (req, res) {
 function remove (req, res) {
     can(req.user.email, req.params.id, data => {
         if(data.error) {
-
+            if(data.error == 'No permission.') res.redirect(`/project/${req.params.id}/1`);
+            else debug(data.error, () => res.redirect('/'));
         } else if(!data.project) res.redirect('/');
         else if(!data.user) res.redirect('/api/auth/login')
         else {
-            
+            let { project } = data;
+            // ...
         }
     }, 'manageTasks');
 }
@@ -85,11 +87,31 @@ function remove (req, res) {
 function update (req, res) {
     can(req.user.email, req.params.id, data => {
         if(data.error) {
-
+            if(data.error == 'No permission.') res.redirect(`/project/${req.params.id}/1`);
+            else debug(data.error, () => res.redirect('/'));
         } else if(!data.project) res.redirect('/');
         else if(!data.user) res.redirect('/api/auth/login')
         else {
-            
+            let { project } = data;
+            let list = project.tasks.find(s=>s.id==req.params.list);
+            if(!list) res.redirect('/project/' + project.id + '/1');
+            else {
+                let task = list.tasks.find(s=>s.id==req.params.tid);
+                if(!task) res.redirect('/project/' + project.id + '/1');
+                else {
+                    if(Object.keys(req.body).length > 0) {
+                        for(let k in req.body) {
+                            if(k == 'due') project.tasks[project.tasks.indexOf(list)].tasks[list.tasks.indexOf(task)][k] = req.body[k];
+                            else if(req.body[k] != '') project.tasks[project.tasks.indexOf(list)].tasks[list.tasks.indexOf(task)][k] = req.body[k];
+                        }
+                        project.markModified('tasks');
+                        project.save(err => {
+                            if(err) debug(err, () => {});
+                            res.redirect('/project/' + project.id + '/1/' + req.params.list + '/' + req.params.tid);
+                        })
+                    } else res.redirect('/project/' + project.id + '/1/' + req.params.list + '/' + req.params.tid);
+                }
+            }
         }
     }, 'manageTasks');
 }
@@ -101,5 +123,5 @@ module.exports.mod = app => {
     app.get('/project/:id/1/:list/:task', auth, getOne)
     app.post('/project/:id/1/create', auth, create)
     app.post('/project/:id/1/remove', auth, remove)
-    app.post('/project/:id/1/update', auth, update)
+    app.post('/project/:id/1/update/:list/:tid', auth, update)
 }
