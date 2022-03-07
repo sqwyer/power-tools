d2882c79545bd3bddd6b1122fb455e605019400e
Sawyer Bivens <sawyer.codes.stuff@gmail.com>
2022-03-07 09:39:51 -0600

    Fix update project method to have permissions

--- server/routes/updateProject.js
+++ server/routes/updateProject.js
@@ -1,11 +1,31 @@
+const { can } = require('../helpers/can');
 const { find } = require('./project');
 
 function post (req, res) {
     let body = req.body;
-    find(req.params.id, _res => {
-        if(_res.error) res.redirect('/dashboard');
-        else {
-            let {project} = _res.data;
+    // find(req.params.id, _res => {
+    //     if(_res.error) res.redirect('/dashboard');
+    //     else {
+    //         let {project} = _res.data;
+    //         for(let [index, [key, value]] of Object.entries(Object.entries(body))) {
+    //             project[key]=value;
+    //             project.markModified(key);
+    //             if((Number(index)+1) == Object.keys(body).length) {
+    //                 project.save(err => {
+    //                     if(err) console.error(err);
+    //                     require('../helpers/redirect')(req, res, `/project/${project._id.toString()}`);
+    //                 });
+    //             }
+    //         }
+    //     }
+    // })
+
+    can(req.user.email, req.params.id, data => {
+        if(data.error) {
+            if(data.error = 'No permission.') require('../helpers/redirect')(req, res, `/project/${project._id.toString()}`);
+            else debug(data.error, () => res.redirect('/'));
+        } else {
+            let {project} = data;
             for(let [index, [key, value]] of Object.entries(Object.entries(body))) {
                 project[key]=value;
                 project.markModified(key);
@@ -17,7 +37,7 @@ function post (req, res) {
                 }
             }
         }
-    })
+    }, "manageProject");
 }
 
 module.exports.mod = app => {
