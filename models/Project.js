5b931d6081ea38ae6af9391f05e2d202beabc8de
Sqwyer <sawyer.codes.stuff@gmail.com>
2022-03-13 15:11:46 -0500

    Fix db connection + start on roles page

--- models/Project.js
+++ models/Project.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const conn = mongoose.createConnection(process.env.MONGO_URI);
+const conn = mongoose.createConnection(process.env.MONGO_URI, {useNewUrlParser: true});
 
 const ProjectSchema = mongoose.Schema({
     name: String,
--- server/project_routes/roles.js
+++ server/project_routes/roles.js
@@ -1,7 +1,16 @@
-// function getRoles (req, res) {
+const { can } = require('../helpers/can');
+const debug = require('../debug');
 
-// }
+function getRoles (req, res) {
+    can(req.user.email, req.params.id, data => {
+        if(data.error) debug(data.error, () => res.redirect('/'));
+        else {
+            let { project, user, role } = data;
+            res.render(`${__dirname}/../../views/project/roles`, { project, user, role });
+        }
+    });
+}
 
-// module.exports.mod = app => {
-//     app.get('/project/:id/4');
-// }
+module.exports.mod = app => {
+    app.get('/project/:id/3', require('../ensureAuth'), getRoles);
+}
