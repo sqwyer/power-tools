2c6123b051a504115321e6df8f180910f5c4f96c
Sqwyer <sawyer.codes.stuff@gmail.com>
2022-03-18 12:33:38 -0500

    Remove backup system

--- server/getModels.js
+++ server/getModels.js
@@ -1,19 +1,10 @@
-if(process.env.NODE_ENV != 'production') require('dotenv').config();
-
 function getModels () {
-    if(process.env.BACKUP === "true") {
-        return {
-            UserModel: require('../backup/User').UserModel,
-            ProjectModel: require('../backup/Project').ProjectModel
-        }
-    } else {
         return {
             UserModel: require('../models/User').UserModel,
             ProjectModel: require('../models/Project').ProjectModel
         }
-    }
 }
 
-getModels.backup = (process.env.BACKUP === "true");
+getModels.backup = false;
 
 module.exports = getModels;
